ce04c8435413e128fcb6264e3388a4f082bd294f
indieka900 <indiekaj@gmail.com>
2023-09-21 11:33:20 +0300

    first commit

---
+++ userApp/models.py
@@ -0,0 +1,41 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.db import models
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(email, password, **extra_fields)
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=30)
+    phone_number = models.CharField(max_length=15)  # Add the phone_number field
+    avatar = models.CharField(max_length=500, blank=True, null=True)  # Add the avatar field
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def __str__(self):
+        return self.email.split('@')[0].upper()
+    
+    def get_short_name(self):
+        return self.full_name.split('@')[0] or self.email.split('@')[0].capitalize()
